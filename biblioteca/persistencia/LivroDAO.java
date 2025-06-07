d773ee17a0c99ae8d6fd5199fa018b51d5f207b8
Luiz Otávio de Almeida <luizotavioodealemida@gmail.com>
2025-06-07 19:15:10 -0300

    Update LivroDAO.java
    Todos métodos essenciais para o CRUD estão presentes
    Código LivroDAO finalizado 
    
    Métodos adicionados
    atulizarLivro
    removerLivro
    mostrarTodosLivros

--- biblioteca/persistencia/LivroDAO.java
+++ biblioteca/persistencia/LivroDAO.java
@@ -9,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class LivroDAO {
 
@@ -81,13 +83,13 @@ public class LivroDAO {
         livro.setAutor(rs.getString("AUTOR"));
         livro.setIsbn(rs.getString("ISBN"));
         livro.setAnoPublic(rs.getInt("ANOPUBLIC"));
-        livro.setQuantidadeDisponivel(rs.getInt("QuantidadeDisponivel"));
+        livro.setQuantidadeDisponivel(rs.getInt("QUANTIDADEDISPONIVEL"));
         livro.setGenero(rs.getString("GENERO"));
 
         return livro;
     }
 
-    // Este método busca um livro no banco de dados usando seu ID.
+// Este método busca um livro no banco de dados usando seu ID.
 // Primeiramente, estabelece a conexão com o banco de dados através da variável 'conexao' (do tipo Connection).
 // Em seguida, prepara a instrução SQL de consulta (SELECT) com um PreparedStatement,
 // que é o comando responsável por obter os dados do livro pelo seu ID.
@@ -138,6 +140,119 @@ public class LivroDAO {
         }
     }
 
+  public List<Livro> listarTodos() throws SQLException {
+
+        //preparamos a conexão
+
+        /* Como que funciona esse código
+        * basicamente em primeiro momento
+        * criamos uma variável para Connection,
+        * PreparedStatement e ResultSet.
+        * Depois disso, definimos ela com null
+        * já que ainda não queremos inicar ela.
+        * Criamos também uma lista para armazenar todos os livros
+        * que serão encotrados dentro do banco de dados, para que, quando
+        * encontra-los os armazenar dentro dessa lista para depois, retornar a própria lista
+        * depois.
+        *
+        * com isso, abrimos um try-cath já que
+        * vamos ter conexão e exeções dentro do código
+        * primeiro criamos a conexão, usando a variável
+        * que recebeu o Connection que no caso é o
+        * conexão, em seguida colocamos que ela é igual a
+        * Conexao, que no caso é a nossa classe que esta lincada
+        * com o banco de dados, com isso conseguimos trazer o método
+        * get.Conexao que irá fazer a conexão com o banco de dados.
+        *
+        * a partir disso criamos o pstmt, que no caso é responsável por ser o
+        * formulário que queremos enviar para o banco dedos.
+        * como nesse caso queremos mostrar todos os livros, usamos o SELECT e os atributo dos livros
+        * depois de deixar pronto esse formulário, preparamos o result set
+        * que é responsável por trazer a resposta do banco de dados para o nosso código
+        * depois de ele ser executado, preenchendo os dados que queremos.
+        *
+        * então, usamos um while, para que enqanto o rs consiga ler o pedido do formulário
+        * será criado um objeto do tipo Livro, que será armazendo então dentro da lista chamada livros.
+        * com isso, irá retornar a lista chamada livros.
+        *
+        *
+        *
+        * */
+
+        Connection conexao = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<Livro>livros = new ArrayList<>(); 
+        try {
+
+            conexao = Conexao.getConexao();
+            String sqlListTodos = "SELECT LIVROID, TITULO, AUTOR, ISBN, ANOPUBLIC, QUANTIDADEDISPONIVEL, GENERO FROM LIVROS";
+            pstmt= conexao.prepareStatement(sqlListTodos);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                Livro livro = criarLivro(rs);
+                livros.add(livro);
+            }
+            return livros;
+
+        }catch (SQLException e){
+            System.err.println("Erro: não foi possível listar todos livros " + e.getMessage());
+            e.printStackTrace();
+            return new ArrayList<>();
+        }finally {
+            DbUtils.close(conexao,pstmt,rs);
+        }
+
+    }
+
+
+    public boolean atualizarLivro(Livro livro) throws SQLException{
+        Connection conexao = null;
+        PreparedStatement pstmt = null;
+        try{
+            conexao= Conexao.getConexao();
+            String sql = "UPDATE LIVROS SET TITULO= ?, AUTOR=?, ISBN=?, ANOPUBLIC=?, QUANTIDADEDISPONIVEL=?, GENERO=? WHERE LIVROID = ? ";
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1,livro.getTitulo());
+            pstmt.setString(2,livro.getAutor());
+            pstmt.setString(3,livro.getIsbn());
+            pstmt.setInt(4,livro.getAnoPublic());
+            pstmt.setInt(5,livro.getQuantidadeDisponivel());
+            pstmt.setString(6,livro.getGenero());
+            pstmt.setInt(7,livro.getLivroId());
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0;
+
+        } catch (SQLException e){
+            System.err.println("Erro ao atualizar livro: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }finally {
+            DbUtils.close(conexao,pstmt,null);
+        }
+    }
+
+    public boolean removerLivro(Livro livro)throws SQLException{
+        Connection conexao = null;
+        PreparedStatement pstmt = null;
+        try{
+            conexao= Conexao.getConexao();
+            String sql = "DELETE FROM LIVROS WHERE LIVROID = ?";
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setInt(1,livro.getLivroId());
+
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e){
+            System.err.println("Erro: Falha ao remover livro: "+ e.getMessage());
+            e.printStackTrace();
+            return false;
+        }finally {
+            DbUtils.close(conexao,pstmt,null);
+        }
+    }
+
+
+
 }
-
-
